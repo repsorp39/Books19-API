99cbd9eb4ba21d18154287c012942167b3ca9f52
Prosper <hazardprosper227@gmail.com>
2025-05-16 15:11:53 +0100

    Update the limiter threshold

--- src/config/rate-limiter.js
+++ src/config/rate-limiter.js
@@ -2,7 +2,7 @@ const { rateLimit } = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  limit: 150,
+  limit: 300,
   message: 'You hit your threshold of requests. Try later',
 });
 
